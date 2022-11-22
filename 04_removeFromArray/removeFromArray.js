edb04ce67615c7023767bb7abfd3716fbf6e41fc
Mohammed Ouzaa <ouzaa.contact@gmail.com>
2022-11-22 19:10:24 +0100

    implementation of the 04 exercise solution

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+    for (const arg of args) {
+        for (const element of array) {
+            if (arg === element) {
+                const index = array.indexOf(element);
+                array.splice(index, 1);
+            }
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
