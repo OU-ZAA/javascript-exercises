4d58d2bf0333ba892a7d17727032f7d205429a6b
ouzaa <ouzaa.contact@gmail.com>
2023-01-12 21:59:14 +0100

    implement exercises 09 solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const reverseStr = string.toLowerCase()
+                             .split("")
+                             .filter((letter) => (letter !== "!" && letter !== " " && letter !== "," && letter !== "."))
+                             .reverse()
+                             .join("")
+                             
+    const str = string.toLowerCase()
+                      .split("")
+                      .filter((letter) => (letter !== "!" && letter !== " " && letter !== "," && letter !== "."))
+                      .join("")
+    return reverseStr === str
 };
 
 // Do not edit below this line
